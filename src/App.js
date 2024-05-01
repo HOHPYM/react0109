8b9384fb454ed052a7c887643ba4dfef10f1f4dd
HOHPYM <142707821+HOHPYM@users.noreply.github.com>
2024-05-01 18:43:56 +0300

    Revert "Сhange №1"
    This reverts commit 5748b497cc754f62a1e549e6048a8adbad203120.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-// App.js
-
-import React, { useState } from 'react';
+import React from 'react';
 import Item from './components/item'; 
-import AddQuestionForm from './components/AddQuestionForm';
 import './App.css';
 
 function App() {
-  const [questions, setQuestions] = useState([]);
-
-  const addQuestion = (newQuestion) => {
-    setQuestions(prevQuestions => [...prevQuestions, newQuestion]);
-  };
-
-  const clearQuestions = () => {
-    setQuestions([]);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <Item questions={questions} />
-        <AddQuestionForm onAddQuestion={addQuestion} />
-        <button onClick={clearQuestions}>Очистить вопросы</button>
+        <Item /> {}
       </header>
     </div>
   );
